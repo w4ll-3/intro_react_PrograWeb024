626fe941b5cdf16ee1bd1c37579111ed8db16cf8
Wellington Cua <wjcua@correo.url.edu.gt>
2024-04-04 11:39:37 -0600

    clase 04042024, uso the cat api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,16 @@
 import Button from "@/components/Button";
+import CatCard from "@/components/CatCard";
 
 export default function Home() {
-  const names = ["Pepito", "Juanito", "Fernandita", "Luisito"];
   return (
     <main>
-      <h1>Hello World</h1>
-      <ul>
-        <li>Primer punto</li>
-        <li>Segundo punto</li>
-        <li>tercer punto</li>
-      </ul>
-      <div className="flex flex-col justify-start items-start">
-        <Button name="Juanito">Hola Juanito</Button>
-        <Button name="Fernandita">Hola Fernandita</Button>
-        <Button name="Luisito">Hola Luisito</Button>
-        <Button name="Pepito">Hola Pepito</Button>
-        <Button>Hola no tengo nombre</Button>
-      </div>
-      <br />
-      <div className="flex flex-col justify-start items-start">
-        {
-          names.map((name, index) => (
-            <Button key={index} name={name}>
-              Hola {name}
-            </Button>
-          ))
-        }
-      </div>
+      <h1>Listado de gatos</h1>
+      <div>
+        <CatCard 
+          id="0XYvRd7oD" 
+          src="https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg"
+        />
+      </div>      
     </main>
   );
 }
---
+++ src/components/CatCard.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+type Props = {
+  id: string;
+  src: string;
+};
+
+export default function CatCard({ id, src }: Props) {
+  return (
+    <div>
+      <Image src={src} alt="Gato" width={300} height={300} />
+      <Link href={`/${id}`}>Ir a perfil</Link>
+    </div>
+  )
+}
