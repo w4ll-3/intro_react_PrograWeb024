a85e23f280aa5bce8aac62d1692117530b00ce13
Wellington Cua <wjcua@correo.url.edu.gt>
2024-04-23 11:48:20 -0600

    clase 23042024: mapeo de 10 tarjetas de gatos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,31 +4,34 @@ import Button from "@/components/Button";
 import CatCard from "@/components/CatCard";
 import { parsedEnv } from "@/parseEnv";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [cats, setCats] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get(parsedEnv.NEXT_PUBLIC_API_URL,{
+      const response = await axios.get(process.env['NEXT_PUBLIC_API_URL'] ?? '',{
         headers: {
-          "x-api-key": parsedEnv.NEXT_PUBLIC_API_KEY,
+          "x-api-key": process.env['NEXT_PUBLIC_API_KEY'],
         },
         }
       );
-      console.log(response.data)
-    }
+      setCats(response.data);
+    };
     fetchData();
   }, []);
   
-
+//cuando las cosas cambian en tiempo de ejecucion se usa un estado
   return (
     <main>
       <h1>Listado de gatos</h1>
       <div>
-        <CatCard 
-          id="0XYvRd7oD" 
-          src="https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg"
-        />
+        {
+          cats.map((cat) => (
+            <CatCard key={cat.id} id={cat.id} src={cat.url} 
+            />
+        ))}
       </div>      
     </main>
   );
